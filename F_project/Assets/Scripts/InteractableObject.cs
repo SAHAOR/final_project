e375c8dfb0aefbd5516ea2a45c30d72d291df44c
SAHAOR <samirhaord@gmail.com>
2025-03-28 18:06:08 -0500

    instancia de objetos pader e hijo segun player

--- F_project/Assets/Scripts/InteractableObject.cs
+++ F_project/Assets/Scripts/InteractableObject.cs
@@ -43,12 +43,40 @@ public class InteractableObject : MonoBehaviourPun
         if (PhotonNetwork.LocalPlayer.ActorNumber == owner)
         {
             photonView.RPC("AddScore", RpcTarget.AllBuffered, owner);
+            RequestInstance();
         }
         else
         {
             photonView.RPC("SubtractScore", RpcTarget.AllBuffered, owner);
         }
+
+         if (!photonView.IsMine) return;
+
     }
+
+    void RequestInstance ()
+    {
+        Debug.Log($"🖱️ {PhotonNetwork.NickName} hizo clic en {gameObject.name}");
+        string prefabName;
+
+        if (gameObject.name.Contains("Apple")) { // Obtener el prefab basado en el nombre del objeto
+            prefabName = "ApplePrefab(Clone)";
+        }
+        else if (gameObject.name.Contains("Banana")) {
+            prefabName = "BananaPrefab(Clone)";
+        }
+        else {
+            Debug.LogError($"❌ No se pudo determinar el prefab de {gameObject.name}");
+            return;
+        }
+
+        Debug.Log(prefabName);
+
+        Vector3 spawnPosition = transform.position + Vector3.up * 0.5f; // Generar la nueva posición un poco arriba del objeto actual
+
+        PlayerController.instance.RequestNewObject(prefabName, spawnPosition); // Llamar a la función de solicitud de instancia en el PlayerController
+    }
+    
 
     [PunRPC]
     void AddScore(int playerID)
