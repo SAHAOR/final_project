a120b0b76e25e99a265eced1b4493595e7888584
SAHAOR <samirhaord@gmail.com>
2025-03-29 22:00:15 -0500

    FInalizando merge con develop

--- F_project/Assets/Scripts/LobbyManager.cs
+++ F_project/Assets/Scripts/LobbyManager.cs
@@ -9,22 +9,14 @@ public class LobbyManager : MonoBehaviourPunCallbacks
     public TMP_InputField roomInput;    
     public TMP_Text roomCodeText;       
     public TMP_Text playersText;        
-<<<<<<< HEAD
     public Button playButton;    
     
-=======
-    public Button playButton;       
->>>>>>> Develop
 
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings(); // Conectar a Photon al iniciar
         PhotonNetwork.AutomaticallySyncScene = true;
         playButton.interactable = false; // Deshabilitar al inicio
-<<<<<<< HEAD
-
-=======
->>>>>>> Develop
     }
 
     public override void OnConnectedToMaster()
@@ -45,11 +37,7 @@ public class LobbyManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-<<<<<<< HEAD
         roomCodeText.text = "Código: " + PhotonNetwork.CurrentRoom.Name;
-=======
-        roomCodeText.text = /*"Código: " +*/ PhotonNetwork.CurrentRoom.Name;
->>>>>>> Develop
         CheckPlayersInRoom();
     }
 
