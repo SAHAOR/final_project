197a00f80c77343f4702e25907238b8e70c3dd71
lucaviro <luiscarlos.villalobosromero@outlook.com>
2025-04-07 15:18:04 -0500

    Se setea botón de regresar al menú en pantalla de creación de salas

--- F_project/Assets/Scripts/BackMenu.cs
+++ F_project/Assets/Scripts/BackMenu.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
+using Photon.Pun;
 
 public class BackMenu : MonoBehaviour
 {
-   
+
     public void ExitToMenu()
     {
         UIManager.instance.CargarEscenaJuego("Felipe");
+        PhotonNetwork.LeaveRoom();
         
     }
 }
